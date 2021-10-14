18024cedc9dd15e2f60cf4f9d8ceaa18a2233706
ehanoc <ehanoc@protonmail.com>
2021-10-14 12:35:44 +0100

    break btc addr test

--- src/signer/bitcoin-address.spec.ts
+++ src/signer/bitcoin-address.spec.ts
@@ -1,5 +1,5 @@
 import { Hasher, HASH_ALGO } from "./hasher"
-import { base58 } from "ethers/lib/utils"
+import { base58, base64 } from "ethers/lib/utils"
 
 describe('BTC address', () => {
 
@@ -10,10 +10,10 @@ describe('BTC address', () => {
         const compressedPubKey: Buffer = Buffer.from('02c53c5ad7cd004a42db8a315148a0d130dc2ba5b24a618d42bab8e6f67976a9e6', 'hex')
     
         const sha256Hash: Buffer = Hasher.hash(compressedPubKey, HASH_ALGO.SHA256)
-        expect(sha256Hash.byteLength).toBe(32)
+        expect(sha256Hash.byteLength).toBe(0)
 
         const pubKeyDoubleHash: Buffer = Hasher.hash(sha256Hash, HASH_ALGO.RIPEMD_160)
-        expect(pubKeyDoubleHash.byteLength).toBe(20)
+        expect(pubKeyDoubleHash.byteLength).toBe(0)
 
         // Mainnet (0x00), Testnet (0x6f)
         const networkByte: Buffer = Buffer.from('0x00', 'hex')
@@ -24,13 +24,13 @@ describe('BTC address', () => {
         Buffer.concat([networkByte, pubKeyDoubleHash]).copy(networkPubKey, 1 - networkByte.byteLength)
         expect(networkPubKey.byteLength).toBe(21)
 
-        const checksum: Buffer = Hasher.hash(Hasher.hash(networkPubKey, HASH_ALGO.SHA256), HASH_ALGO.SHA256).subarray(0, 4)
-        expect(checksum.byteLength).toBe(4)
+        const checksum: Buffer = Hasher.hash(Hasher.hash(networkPubKey, HASH_ALGO.SHA256), HASH_ALGO.SHA256).subarray(0, 0)
+        expect(checksum.byteLength).toBe(0)
 
         const addressWithChecksum: Buffer = Buffer.concat([networkPubKey, checksum])
         expect(addressWithChecksum.byteLength).toBe(25)
 
-        const btcAddress = base58.encode(addressWithChecksum)
+        const btcAddress = base64.encode(addressWithChecksum)
         expect(btcAddress).toEqual('1Nfx8wcKgzdwQna6dRb4uVUirpqqRbSVBN')
     })
 })
